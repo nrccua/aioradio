50d3bd119da75b8bb03d8b5ac081ebd704e5b390
Tim Reichard <tim.reichard@encoura.org>
2024-07-25 17:11:49 -0500

    fix all dependency issues

--- HISTORY.rst
+++ HISTORY.rst
@@ -3,6 +3,21 @@ History
 =======
 
 
+v0.20.23 (2024-07-25)
+
+* Update install requires grpcio==1.62.2 & grpcio-status==1.62.2.
+
+
+v0.20.22 (2024-07-25)
+
+* Update install require numpy==1.26.4.
+
+
+v0.20.21 (2024-07-25)
+
+* Fix broken dependencies.
+
+
 v0.20.20 (2024-07-25)
 
 * Update mlflow, flask, flask-cors & werkzeug to remove vulnerability alert.
--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ projects."""
 
 from setuptools import setup
 
-with open('README.md', 'r') as fileobj:
+with open('README.md', 'r', encoding='utf8') as fileobj:
     long_description = fileobj.read()
 
 setup(name='aioradio',
-    version='0.20.20',
+    version='0.20.23',
     description='Generic asynchronous i/o python utilities for AWS services (SQS, S3, DynamoDB, Secrets Manager), Redis, MSSQL (pyodbc), JIRA and more',
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -21,23 +21,27 @@ setup(name='aioradio',
     ],
     install_requires=[
         'cython>=0.29.33',
-        'aioboto3==12.4.0',
+        'aioboto3==13.1.1',
         'aiojobs>=1.0.0',
         'backoff>=2.1.2',
-        'botocore==1.34.69',
-        'boto3==1.34.69',
+        'botocore==1.34.131',
+        'boto3==1.34.131',
         'ddtrace>=0.60.1',
         'faust-cchardet>=2.1.18',
         'fakeredis>=2.20.0',
+        'grpcio==1.62.2',
+        'grpcio-status==1.62.2',
         'haversine>=2.8.0',
         'httpx>=0.23.0',
         'mandrill>=1.0.60',
         'mlflow>=2.10.2',
-        'numpy>=1.19',
+        'numpy==1.26.4',
         'openpyxl==3.0.10',
         'orjson>=3.6.8',
         'pandas>=1.3.5',
+        'pkginfo==1.10.0'
         'polars>=0.19.12',
+        'protobuf==4.25.4',
         'pyarrow>=13.0.0',
         'pysmb>=1.2.7',
         'python-json-logger>=2.0.2',
@@ -45,16 +49,16 @@ setup(name='aioradio',
     ],
     include_package_data=True,
     tests_require=[
-        'flask==2.1.2',
-        'flask-cors>=3.0.10',
-        'moto==3.1.18',
+        'flask==3.0.3',
+        'flask-cors>=4.0.1',
+        'moto==4.2.14',
         'pre-commit>=2.15.0',
         'pylint>=2.13.8',
         'pytest>=7.0.1',
         'pytest-asyncio>=0.15.1',
         'pytest-cov>=3.0.0',
         'typing_extensions>=4.10.0',
-        'werkzeug==2.1.2'
+        'werkzeug==3.0.3'
     ],
     zip_safe=False,
     classifiers=[
